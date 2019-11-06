7f8daaafb7144f35d6c981714671f7238de223b4
joabland <joabland@bancolombia.com.co>
2019-11-06 00:15:36 -0500

    Se eliminaron Target que no se estaban utilizando

--- src/main/java/co/com/tongo/automatizacion/page/ConsultaProcesosPage.java
+++ src/main/java/co/com/tongo/automatizacion/page/ConsultaProcesosPage.java
@@ -9,7 +9,4 @@ public class ConsultaProcesosPage extends PageObject {
     public static final Target SEL_ENTIDAD = Target.the("Select box de entidad").located(By.id("ddlEntidadEspecialidad"));
     public static final Target INPUT_RADICADO = Target.the("Input radicado").locatedBy("//*[@id=\"divNumRadicacion\"]//input[1]");
     public static final Target TEXT_BARRA = Target.the("Text barra").located(By.id("ConsultarNum"));
-    public static final Target SEL_OPCION_DE_CONSULTA = Target.the("Select opcion de consulta").located(By.id("rblConsulta"));
-    public static final Target SLIDER_I_AM_HUMAN = Target.the("Slider test I am Human").locatedBy("//*[@id=\"sliderBehaviorNumeroProceso_railElement\"]/div");
-    public static final Target TEXT_TIPO_SUJETO = Target.the("Texto tipo sujeto").located(By.id("lblSujeto1"));
 }
