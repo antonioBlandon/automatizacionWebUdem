e3127753beb668dde02cb60e7ffacd6bb474dece
joabland <joabland@bancolombia.com.co>
2019-11-06 00:06:27 -0500

    Se cambio el xpath de sel ciudad que era el que estaba haciendo que fallara la automatizacion

--- src/main/java/co/com/tongo/automatizacion/page/ConsultaProcesosPage.java
+++ src/main/java/co/com/tongo/automatizacion/page/ConsultaProcesosPage.java
@@ -5,7 +5,7 @@ import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
 public class ConsultaProcesosPage extends PageObject {
-    public static final Target SEL_CIUDAD = Target.the("Select box de ciudad").located(By.id("ddlciudad"));
+    public static final Target SEL_CIUDAD = Target.the("Select box de ciudad").located(By.id("ddlCiudad"));
     public static final Target SEL_ENTIDAD = Target.the("Select box de entidad").located(By.id("ddlEntidadEspecialidad"));
     public static final Target INPUT_RADICADO = Target.the("Input radicado").locatedBy("//*[@id=\"divNumRadicacion\"]//input[1]");
     public static final Target TEXT_BARRA = Target.the("Text barra").located(By.id("ConsultarNum"));
